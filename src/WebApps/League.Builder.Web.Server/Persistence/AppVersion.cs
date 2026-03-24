67945ab9395f7dd56192620f47f744ccd26dbb39
Anthony Piccirilli <anthony.n.piccirilli.ctr@army.mil>
2026-03-24 16:22:00 -0400

    - add versioning to application

---
+++ src/WebApps/League.Builder.Web.Server/Persistence/AppVersion.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace League.Builder.Web.Server.Persistence;
+
+public static class AppVersion
+{
+    public static string Full =>
+        Assembly.GetExecutingAssembly()
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion
+        ?? "Unknown";
+
+    public static string Short =>
+        Full.Contains('+')
+            ? $"{Full.Split('+')[0]} ({Full.Split('+')[1][..7]})"
+            : Full;
+}
